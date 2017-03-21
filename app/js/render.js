64c521ba6215a6a67e94f9c0d6217d058460eb2f
Arnaud Crowther <hi@arnaud.cr>
2017-03-20 22:11:57 -0500

    added zip decompress, modal, double click, file search

--- app/js/render.js
+++ app/js/render.js
@@ -1,10 +1,100 @@
-var DecompressZip = require('decompress-zip');
-var app = require('electron').remote;
-var dialog = app.dialog;
+/* todo
+*
+* - make sure only zip archive is accepted
+*
+*
+*/
 
 
 (function () {
   
+  var profiles = [];
+  
+  function getTempPath() {
+    const {app} = require('electron').remote;
+    var tempPath = app.getPath('userData')+"/tmp";
+    return tempPath;
+  }
+  
+  
+  
+  //drag the archive in and decompress it
+  var holder = document.getElementById('drag-file');
+  holder.ondragover = () => { return false; };
+  holder.ondragleave = () => { return false; };
+  holder.ondragend = () => { return false; };
+  holder.ondrop = (e) => {
+    e.preventDefault();
+    for (let f of e.dataTransfer.files) {
+      uncompress(f.path, getTempPath());
+//      openModal()
+      getSystemProfile();
+    }
+    return false;
+  };
+  
+  
+  
+  //double click the window to pick a file
+  var app = require('electron').remote;
+  var dialog = app.dialog;
+  document.getElementById("drag-file").addEventListener("dblclick", () => {
+    dialog.showOpenDialog({
+      title:"Select the .zip file to decompress"
+    },function (fileNames) {
+      if(fileNames === undefined) {
+        console.log("No file selected");
+        return;
+      } else {
+        uncompress(fileNames[0], getTempPath());
+      }
+    });          
+  }, false);
+  
+  
+  
+  //decompress the zip file
+  var DecompressZip = require('decompress-zip');
+  function uncompress(ZIP_FILE_PATH, DESTINATION_PATH) {
+    var unzipper = new DecompressZip(ZIP_FILE_PATH);
+    unzipper.on('progress', function (fileIndex, fileCount) {//extraction progress
+//      console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
+    });
+    unzipper.on('extract', function (log) {//extracting completed sucessfully
+//      console.log('Finished extracting', log);
+    });
+    unzipper.on('error', function (err) {//error event listener
+//      console.log('Caught an error', err);
+    });
+    unzipper.extract({ //unzip
+      path: DESTINATION_PATH
+    });
+  }
+  
+  
+  
+  //get the system profile names and locations
+  function getSystemProfile() {
+    
+    var readdirp = require('readdirp'); 
+    readdirp({ root: getTempPath()+"/Server", fileFilter: '*.profile.xml'}, function (errors, res) {
+      if (errors) {
+        errors.forEach(function (err) {
+          console.error('Error: ', err);
+        });
+      }
+      $.each(res.files, function( index, value ) {
+        if (value.name !== "Monitoring.profile.xml" && value.name !== "dynaTrace Self-Monitoring.profile.xml") {
+          profiles.push(value.path)
+        }
+      });
+    });
+    console.log(profiles)
+  }
+  
+  
+  
+  //open modal and pass data for printout list
   function openModal() {
     const BrowserWindow = require('electron').remote.BrowserWindow
     const path = require('path')
@@ -14,55 +104,6 @@ var dialog = app.dialog;
     win.loadURL(modalPath)
     win.show()
   }
-
-
-  function uncompress(ZIP_FILE_PATH, DESTINATION_PATH){
-    var unzipper = new DecompressZip(ZIP_FILE_PATH);
-    // Add the error event listener
-    unzipper.on('error', function (err) {
-      console.log('Caught an error', err);
-    });
-    // Notify when everything is extracted
-    unzipper.on('extract', function (log) {
-      console.log('Finished extracting', log);
-    });
-    // Notify "progress" of the decompressed files
-    unzipper.on('progress', function (fileIndex, fileCount) {
-      console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
-    });
-    // Unzip !
-    unzipper.extract({
-      path: DESTINATION_PATH
-    });
-  }
-
-
-  var holder = document.getElementById('drag-file');
-  holder.ondragover = () => {
-    return false;
-  };
-  holder.ondragleave = () => {
-    return false;
-  };
-  holder.ondragend = () => {
-    return false;
-  };
-  holder.ondrop = (e) => {
-    e.preventDefault();
-    for (let f of e.dataTransfer.files) {
-      uncompress(f.path,"tmp");
-      openModal()
-//      console.log(f)
-//      console.log(f.path)
-//      console.log(e.dataTransfer)
-//      console.log(e)
-//      console.log('File(s) you dragged here: ', f.path)
-    }
-    return false;
-  };
+  
+  
 })();
-
-
-
-
-
--- index.js
+++ index.js
@@ -1,53 +1,52 @@
 'use strict';
 const electron = require('electron');
-
 const app = electron.app;
 
-//const remote = require('remote'); 
-//const dialog = remote.require('dialog');
-//const fs = require('fs');
+//console.log(app.getPath('userData'))
 
-// adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
+require('electron-debug')({showDevTools: true});
+
+
 
 // prevent window being garbage collected
 let mainWindow;
 
 function onClosed() {
-	// dereference the window
-	// for multiple windows store them in an array
-	mainWindow = null;
+  // dereference the window
+  mainWindow = null;
 }
 
 function createMainWindow() {
-	const win = new electron.BrowserWindow({
-		width: 400,
-		height: 400,
-        titleBarStyle: 'hidden',
-        resizable: false
-//      titleBarStyle: 'hidden-inset'
-//      transparent: true, frame: false
-//        'standard-window': false
-	});
-
-	win.loadURL(`file://${__dirname}/app/index.html`);
-	win.on('closed', onClosed);
-
-	return win;
+  const win = new electron.BrowserWindow({
+    width: 400,
+    height: 400,
+    title: "Pro-File",
+    resizable: false,
+    fullscreen: false,
+//    frame: false,
+//    titleBarStyle: 'hidden',
+//    resizable: false,
+//    titleBarStyle: 'hidden-inset',
+//    transparent: true, frame: false,
+//    'standard-window': false
+  });
+  win.setTitle(require('./package.json').name);
+  win.loadURL(`file://${__dirname}/app/index.html`);
+  win.on('closed', onClosed);
+  return win;
 }
 
 app.on('window-all-closed', () => {
-//	if (process.platform !== 'darwin') {
-		app.quit();
-//	}
+//  if (process.platform !== 'darwin') {app.quit();}
+  app.quit();
 });
 
 app.on('activate', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
+  if (!mainWindow) {
+    mainWindow = createMainWindow();
+  }
 });
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
+  mainWindow = createMainWindow();
 });
